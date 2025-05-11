b24433c82501ba39f07f44d2f2bf6fb5f86f9cfe
digkill <digkill@mail.ru>
2025-05-12 01:22:42 +0500

    Additional features

--- internal/models/user.go
+++ internal/models/user.go
@@ -9,8 +9,8 @@ import (
 
 type User struct {
 	ChatID         int64          `db:"id"`
-	StartDate      time.Time      `db:"start_date"`
-	LastLessonSent int            `db:"last_lessonSent"`
+	StartDate      string         `db:"start_date"`
+	LastLessonSent sql.NullInt32  `db:"last_lessonSent"`
 	CreatedAt      sql.NullString `db:"created_at"`
 	UpdatedAt      sql.NullString `db:"updated_at"`
 }
